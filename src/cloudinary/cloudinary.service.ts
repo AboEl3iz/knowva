581f8c2eb30c1033843461294329cea2bf46b2c0
AboEl3iz <k.hassan202077@gmail.com>
2025-09-09 10:16:15 +0300

    fix bug in lesson

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -5,20 +5,29 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class CloudinaryService {
     async uploadFile(file: Express.Multer.File, folder?: string): Promise<any> {
-        return new Promise((resolve, reject) => {
-            const uploadStream = cloudinary.uploader.upload_stream(
-                { folder: folder || 'lessons' , resource_type : 'raw' , access_mode : 'public' , format : 'pdf' },
-                 // optional folder
-                (error, result) => {
-                    if (error) return reject(error);
-                    resolve(result);
-                },
-            );
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        folder: folder || 'lessons',
+        resource_type: 'raw',
+        access_mode: 'public',
+      },
+      (error, result) => {
+        if (error) return reject(error);
 
-            // convert buffer to stream
-            Readable.from(file.buffer).pipe(uploadStream);
+        // نبني رابط التحميل بإسم وامتداد محدد
+        const downloadUrl = 'https://res.cloudinary.com/${cloudinary.config().cloud_name}/raw/upload/fl_attachment:${result.public_id}.pdf/v${result.version}/${result.public_id}.pdf';
+
+        resolve({
+          ...result,
+          downloadUrl, // اللينك اللي بينزل كـ PDF بالامتداد
         });
-    }
+      },
+    );
+
+    Readable.from(file.buffer).pipe(uploadStream);
+  });
+}
 
     async deleteFile(publicId: string): Promise<any> {
         return cloudinary.uploader.destroy(publicId);
