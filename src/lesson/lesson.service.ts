744cf31d9003617ba654f46662f104f0facd17dd
AboEl3iz <k.hassan202077@gmail.com>
2025-09-11 15:26:36 +0300

    solve the lesson problem to upload files pdf

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -48,7 +48,7 @@ export class LessonService {
       throw new BadRequestException("One or more groups not found");
     }
 
-    const {secure_url , downloadUrl ,public_id} = await this.cloudinaryService.uploadFile(file).catch((err) => {
+    const result = await this.cloudinaryService.uploadLessonFile(file).catch((err) => {
       console.log(err);
       throw new BadRequestException('Error uploading file');
     });
@@ -70,10 +70,10 @@ export class LessonService {
         createdById: userId,
         title: CreateLessonDto.title,
         description : CreateLessonDto.description,
-        url:  file.mimetype === 'application/pdf' ? downloadUrl : secure_url,
+        url:  result.secure_url,
         type: type,
         subjectId: subjectId,
-        publicId: public_id,
+        publicId: result.public_id,
 
       }
     });
