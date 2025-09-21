46b910dfc149844ec76808c66d7006e7bc284d1f
AboEl3iz <k.hassan202077@gmail.com>
2025-09-21 18:07:19 +0300

    overwrite on chatbot schema

--- src/chatbot/chatbot.controller.ts
+++ src/chatbot/chatbot.controller.ts
@@ -21,8 +21,17 @@ export class ChatbotController {
     @ApiOperation({ summary: 'Get all chatbots' })
     @ApiResponse({
         status: 200,
-        description: 'Get all chatbots',
-        type: [ChatbotResponseDto]
+        example: [
+            [
+                {
+                    "id": 1,
+                    "title": "calculate this equition 4*4 = ?",
+                    "groupId": 5,
+                    "studentId": 7,
+                    "createdAt": "2025-09-21T13:32:16.950Z"
+                }
+            ]
+        ]
     })
     @ApiResponse({ status: 401, description: 'Unauthorized' })
     @ApiResponse({ status: 403, description: 'Forbidden' })
@@ -37,8 +46,57 @@ export class ChatbotController {
     @ApiParam({ name: 'sessionId', type: 'number', required: true, description: 'Chatbot session id' })
     @ApiResponse({
         status: 200,
-        description: 'Get chatbot',
-        type: ChatbotWithMessagesResponseDto
+        example: {
+            "id": 1,
+            "title": "calculate this equition 4*4 = ?",
+            "groupId": 5,
+            "studentId": 7,
+            "createdAt": "2025-09-21T13:32:16.950Z",
+            "messages": [
+                {
+                    "id": 1,
+                    "text": "calculate this equition 4*4 = ?",
+                    "sessionId": 1,
+                    "aiGenerated": false,
+                    "createdAt": "2025-09-21T14:44:29.992Z"
+                },
+                {
+                    "id": 2,
+                    "text": "4 * 4 = 16\n",
+                    "sessionId": 1,
+                    "aiGenerated": true,
+                    "createdAt": "2025-09-21T14:44:29.992Z"
+                },
+                {
+                    "id": 3,
+                    "text": "calculate this equition 4*8 = ?",
+                    "sessionId": 1,
+                    "aiGenerated": false,
+                    "createdAt": "2025-09-21T14:50:51.134Z"
+                },
+                {
+                    "id": 4,
+                    "text": "4 * 8 = 32\n",
+                    "sessionId": 1,
+                    "aiGenerated": true,
+                    "createdAt": "2025-09-21T14:50:51.134Z"
+                },
+                {
+                    "id": 5,
+                    "text": "calculate this equition 4*9 = ?",
+                    "sessionId": 1,
+                    "aiGenerated": false,
+                    "createdAt": "2025-09-21T14:54:13.249Z"
+                },
+                {
+                    "id": 6,
+                    "text": "4 * 9 = 36\n",
+                    "sessionId": 1,
+                    "aiGenerated": true,
+                    "createdAt": "2025-09-21T14:54:13.249Z"
+                }
+            ]
+        }
     })
     @ApiResponse({ status: 401, description: 'Unauthorized' })
     @ApiResponse({ status: 403, description: 'Forbidden' })
@@ -69,7 +127,15 @@ export class ChatbotController {
     @UseGuards(AuthenticationGuard, AuthorizationGuard)
     @ApiOperation({ summary: 'Send message' })
     @ApiParam({ name: 'sessionId', type: 'number', required: true, description: 'Chatbot session id' })
-    @ApiBody({ description: 'Message', type: String, required: true })
+    @ApiBody({
+        schema: {
+            type: 'object',
+            properties: {
+                text: { type: 'string', example: 'Hello, how are you?' }
+            },
+            required: ['text']
+        }
+    })
     @ApiResponse({
         status: 200,
         description: 'Send message',
@@ -79,7 +145,7 @@ export class ChatbotController {
     @ApiResponse({ status: 403, description: 'Forbidden' })
     @ApiResponse({ status: 404, description: 'Chatbot session not found' })
     @ApiResponse({ status: 400, description: 'Failed to send message' })
-    async sendMessage(@Req() req, @Param('sessionId') sessionId: number, @Body() message: { text: string }) {
-        return await this.ChatbotService.sendMessage(+req.user.id, message.text, sessionId);
+    async sendMessage(@Req() req, @Param('sessionId') sessionId: string, @Body() message: { text: string }) {
+        return await this.ChatbotService.sendMessage(+req.user.id, message.text, +sessionId);
     }
 }
--- src/chatbot/chatbot.service.ts
+++ src/chatbot/chatbot.service.ts
@@ -1,5 +1,6 @@
 import { BadRequestException, Injectable, Logger } from "@nestjs/common";
 import axios from "axios";
+import { group } from "console";
 import { PrismaService } from "src/database/prisma.service";
 @Injectable()
 export class ChatbotService {
@@ -18,33 +19,46 @@ export class ChatbotService {
         if (!membership) {
             throw new BadRequestException('User is not a member of the group');
         }
+        
 
         let session = await this.prisma.chatbotSession.create({ data: { studentId: userId, groupId } });
         return session;
     }
 
     async sendMessage(userId: number, message: string, sessionId: number) {
-        const session = await this.prisma.chatbotSession.findUnique({
+        const session = await this.prisma.chatbotSession.findFirst({
             where: { id: sessionId, studentId: userId },
-            include: { messages: true , group: true }
+            include: { messages: true, group: true }
         })
         if (!session) {
             throw new BadRequestException('Session not found');
         }
         Logger.debug(`Sending message to chatbot: ${message} for session ${sessionId} and group ${session.groupId}`);
         // message = message.trim();
+        // let requestPayload = {
+        //     group_id: session.groupId.toString(),
+        //     user_query: message,
+        //     student_id: userId.toString(),
+        //     session_id: sessionId.toString()
+
+        // }
+        let requestPayload = {
+            group_id: session.group.id.toString(),
+            user_query: message,
+            student_id: "1",
+            session_id: session.id.toString()
+        };
+
+
 
         let response;
         try {
-            response = await axios.post('https://8080-01k4nxc27xwgyn4vsge9kda40b.cloudspaces.litng.ai/ai/chatbot/ask', {
-                group_id: session.groupId.toString(),
-                user_query: message,
-                student_id: userId.toString(),
-                session_id: sessionId.toString()
-            });
+            Logger.debug(`Request payload: ${JSON.stringify(requestPayload)}`);
+
+            response = await axios.post('https://8080-01k4nxc27xwgyn4vsge9kda40b.cloudspaces.litng.ai/ai/chatbot/ask', requestPayload, { headers: { 'Content-Type': 'application/json' } });
             Logger.debug(`Chatbot response: ${JSON.stringify(response.data)}`);
         } catch (e) {
-            throw new BadRequestException(`Failed to send message to chatbot: ${e.message}` );
+            throw new BadRequestException(`Failed to send message to chatbot: ${e.message}`);
         }
 
         const answer = response.data.response.answer;
